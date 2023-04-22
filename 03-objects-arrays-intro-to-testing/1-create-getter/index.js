f95dd1768c7e9c66e59f259ec4e3b13047240a9e
unknown <pochtalexa@yandex.ru>
2023-04-22 20:00:40 +0300

    03-objects-arrays-intro-to-testing

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const pathList = path.split('.');
+  return function (obj) {
+    let tmpObj = obj;
+    for (const el of pathList) {
+      if (!tmpObj.hasOwnProperty(el)) {
+        return undefined;
+      }
+      tmpObj = tmpObj[el];
+    }
+    return tmpObj;
+  };
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -1,8 +1,11 @@
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
+
 /**
  * uniq - returns array of uniq values:
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+  const result = new Set(arr);
+  return Array.from(result);
 }
